10e8ae98ebcab2702a4012a55033a95af73ec67f
Sam Coleman <scoleman@olin.edu>
2022-04-17 21:40:09 -0400

    Change edgelist mtx files to txt, create NetX graph

--- file_to_nx.py
+++ file_to_nx.py
@@ -1,3 +1,18 @@
 """
 Reads in a data file formated as a list of edges, and builds a networx graph
 """
+
+import networkx as nx
+
+def create_graph(file_path, delim = None):
+    """
+    Return a NetworkX Graph from edgelist
+
+    Args:
+        file_path: A string representing path to txt file/
+        delim: A string representing the delimiter between the two nodes
+
+    Returns:
+        A NetworkX undirected graph
+    """
+    return nx.read_edgelist(file_path, delimiter = delim)

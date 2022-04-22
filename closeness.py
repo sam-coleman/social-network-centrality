eca5951cdfb6d85bbf5b97cbd4d18149d3dada81
Sam Coleman <scoleman@olin.edu>
2022-04-21 20:55:22 -0400

    Clean closeness and degree

--- closeness.py
+++ closeness.py
@@ -2,7 +2,6 @@
 Our implementation for closeness centrality
 """
 
-from tracemalloc import start
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -12,22 +11,29 @@ from plotting import *
 G = create_graph('data/soc-dolphins/soc-dolphins.txt', delim = ' ')
 
 def closeness_centrality(G):
-    # implementing the simplified case from initial research
-    #nx.shortest_path(G, source = 2, target = 4))
-    num_nodes = G.number_of_nodes()
+    """
+    Return closeness centrality measure for all nodes in a graph.
+
+    Args:
+        G: A NetworkX Graph
+    
+    Returns: 
+        A dictionary with node:closeness_centrality for each node in G.
+    """
+    NUM_NODES = G.number_of_nodes()
     val_map = {}
     for start_node in G.nodes():
-        l_paths = 0
+        l_all_paths = 0 # sum of lengths of shortest path for start_node
         for end_node in G.nodes():
             if start_node != end_node:
-                path = nx.shortest_path(G, start_node, end_node)
-                len_path = len(path) - 1
-                l_paths += len_path
+                path = nx.shortest_path(G, start_node, end_node) # get shortest path
+                len_path = len(path) - 1 # get number of edges in shortest path
+                l_all_paths += len_path
         
-        val_map[start_node] = (num_nodes - 1) / l_paths
+        val_map[start_node] = (NUM_NODES - 1) / l_all_paths
     
     return val_map
 
 
 val_map = closeness_centrality(G)
-plot_colormap(G, nx_val_map)
+plot_colormap(G, val_map)
--- degree.py
+++ degree.py
@@ -11,13 +11,22 @@ from plotting import *
 G = create_graph('data/soc-dolphins/soc-dolphins.txt', delim = ' ')
 
 def degree_centrality(G):
+    """
+    Return degree centrality measure for all nodes in a graph.
+
+    Args:
+        G: A NetworkX Graph
+    
+    Returns: 
+        A dictionary with node:degree_centrality for each node in G.
+    """
     val_map = {}
-    num_nodes = G.number_of_nodes()
+    NUM_NODES = G.number_of_nodes()
     deg_list = G.degree() # list of tuples
     for n in deg_list:
-        val_map[n[0]] = n[1] / num_nodes
+        val_map[n[0]] = n[1] / NUM_NODES
+        
     return val_map
-
 
 val_map = degree_centrality(G)
 plot_colormap(G, val_map)

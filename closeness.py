8e189fb7baacef5fab55202866515e36a60413ec
Sam Coleman <scoleman@olin.edu>
2022-04-21 20:44:47 -0400

    Implement closeness centrality

--- closeness.py
+++ closeness.py
@@ -2,6 +2,7 @@
 Our implementation for closeness centrality
 """
 
+from tracemalloc import start
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -11,7 +12,22 @@ from plotting import *
 G = create_graph('data/soc-dolphins/soc-dolphins.txt', delim = ' ')
 
 def closeness_centrality(G):
-    pass
+    # implementing the simplified case from initial research
+    #nx.shortest_path(G, source = 2, target = 4))
+    num_nodes = G.number_of_nodes()
+    val_map = {}
+    for start_node in G.nodes():
+        l_paths = 0
+        for end_node in G.nodes():
+            if start_node != end_node:
+                path = nx.shortest_path(G, start_node, end_node)
+                len_path = len(path) - 1
+                l_paths += len_path
+        
+        val_map[start_node] = (num_nodes - 1) / l_paths
+    
+    return val_map
 
-# val_map = closeness_centrality(G)
-# plot_colormap(G, val_map)
+
+val_map = closeness_centrality(G)
+plot_colormap(G, nx_val_map)
